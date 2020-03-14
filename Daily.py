2309b28cd7daedcd3709611137c35a28518895fa
Tab <41122304+Tabu-n@users.noreply.github.com>
2020-03-14 18:09:06 +0800

    Add files via upload

---
+++ Daily.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+url="https://thos.tsinghua.edu.cn/"
+
+#edge_opt = Options()      # 创建参数设置对象.
+#edge_opt.add_argument('--headless')   # 无界面化.
+#edge_opt.add_argument('--disable-gpu')    # 配合上面的无界面化.
+driver = webdriver.Edge()
+driver.implicitly_wait("10")
+driver.get(url)
+
+#登录
+driver.find_element_by_link_text("登录").click()
+driver.implicitly_wait("20")
+
+#日报
+driver.find_element_by_name("【日报】学生健康和出行情况报告").click()
+
+#提交
+time.sleep(15)
+driver.find_element_by_id("commit").click()
+
+driver.quit()
